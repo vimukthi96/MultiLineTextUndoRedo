a16fd3b464e1424e5e6e736667595963bc5a0342
vimukthi96 <vimukthi072@gmail.com>
2018-11-16 18:46:25 +0530

    add menu bar button

--- app/src/main/java/com/example/vimukthi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/vimukthi/myapplication/MainActivity.java
@@ -2,38 +2,110 @@ package com.example.vimukthi.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.MultiAutoCompleteTextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    MultiAutoCompleteTextView editText;
+    Button redoBtn;
+    Button undoBtn;
+    TextViewUndoRedo helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button redoBtn = (Button) findViewById(R.id.redo);
-        Button undoBtn = (Button) findViewById(R.id.undo);
-        MultiAutoCompleteTextView editText = (MultiAutoCompleteTextView) findViewById(R.id.edittext);
+        redoBtn = (Button) findViewById(R.id.redo);
+        undoBtn = (Button) findViewById(R.id.undo);
+        undoBtn.setEnabled(false);
+        redoBtn.setEnabled(false);
+        editText = (MultiAutoCompleteTextView) findViewById(R.id.edittext);
+        helper = new TextViewUndoRedo(editText);
 
-        // pass edittext object to TextViewUndoRedo class
-        final TextViewUndoRedo helper = new TextViewUndoRedo(editText);
-
-        // call the method from TextViewUndoRedo class
         redoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                helper.redo(); // perform redo
+                performRedo();
             }
         });
         undoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                helper.undo();
+                if (textInType() == true) {
+
+                    performUndo();
+                }
+                redoBtn.setEnabled(true);
+            }
+        });
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                textInType();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
 
-
     }
+
+    public boolean textInType() {
+        if (editText.getText().length() == 0) {
+            undoBtn.setEnabled(false);
+            redoBtn.setEnabled(false);
+            return false;
+        } else {
+            undoBtn.setEnabled(true);
+            return true;
+
+        }
+    }
+
+
+    private void performRedo() {
+        helper.redo();
+    }
+
+    private void performUndo() {
+        helper.undo();
+    }
+
+    ImageButton locButton;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.new_file_view, menu);
+        locButton = (ImageButton) menu.findItem(R.id.action_undo_btn).getActionView();
+        locButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Toast.makeText(MainActivity.this,"edjebdre",Toast.LENGTH_SHORT).show();
+              //  performUndo();
+           }
+    });
+        return true;
+    }
+
+
+
+
 }
+
